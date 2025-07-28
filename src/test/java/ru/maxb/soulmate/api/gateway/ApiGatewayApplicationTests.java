cc413a0c63fc2b33b22091e0ddb8d64393e66ec5
mbuyanov <mbuyanov@roox.ru>
2025-07-28 18:34:22 +0300

    Init project

---
+++ src/test/java/ru/maxb/soulmate/api/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maxb.soulmate.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
